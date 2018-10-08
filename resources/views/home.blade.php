e02498fb3f63352173432ce979960494661af722
keiji19 <keijihashike@gmail.com>
2018-10-09 03:56:43 +0900

    WIP:トップページ作成

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SAKENO</title>
+
+    <link href="css/common.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+</head>
+
+<body id="index">
+    <header>
+        <div class="logo">
+            <a href="{{ url('/') }}">
+                <p>Logo</p>
+            </a>
+        </div>
+        <nav>
+            <ul class="global-nav">
+                <li>
+                    <a href="#">サービスについて</a>
+                </li>
+                <li>
+                    <a href="#">コンタクト</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main style=" height: 570px;">
+    </main>
+    <footer>Copyright hashimoto.keiji</footer>
+</body>
+
+</html>
